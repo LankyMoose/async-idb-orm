14d7cbafe7c6cd0b5f28d5cee53bbf81370f5685
Robby6Strings <rausten93@gmail.com>
2023-10-21 02:09:28 +1300

    tweaks

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,40 +2,36 @@ import { idb } from "./idb.js"
 import { Field, model } from "./model.js"
 export { idb, Field, model }
 
-const users = model("User", {
-  id: Field.number({ primaryKey: true }),
-  name: Field.string({ default: "John Doe" }),
-  age: Field.number({ index: true }),
-  birthday: Field.date({ default: () => new Date() }),
-  pets: Field.array(
-    model("Pet", {
-      name: Field.string({ optional: true }),
-      age: Field.number(),
-      species: Field.string(),
-      birthday: Field.date({ default: () => new Date() }),
-    })
-  ),
-  alive: Field.boolean(),
-})
+// const users = model("User", {
+//   id: Field.number({ primaryKey: true }),
+//   name: Field.string({ default: "John Doe" }),
+//   age: Field.number({ index: true }),
+//   birthday: Field.date({ default: () => new Date() }),
+//   pets: Field.array(
+//     model("Pet", {
+//       name: Field.string({ optional: true }),
+//       age: Field.number(),
+//       species: Field.string(),
+//       birthday: Field.date({ default: () => new Date() }),
+//     })
+//   ),
+//   alive: Field.boolean(),
+// })
 
-const db = idb("demo", { users })
+// const db = idb("demo", { users })
 
-async function addUser() {
-  const user = await db.users.create({
-    id: 1,
-    age: 25,
-    pets: [
-      {
-        age: 1,
-        species: "dog",
-      },
-    ],
-    alive: true,
-  })
-
-  return user
-}
-
-addUser().then((user) => {
-  console.log(user!.pets[0]!.birthday)
-})
+// db.users
+//   .create({
+//     id: 1,
+//     age: 25,
+//     pets: [
+//       {
+//         age: 1,
+//         species: "dog",
+//       },
+//     ],
+//     alive: true,
+//   })
+//   .then((user) => {
+//     console.log(user!.pets[0]!.birthday)
+//   })
